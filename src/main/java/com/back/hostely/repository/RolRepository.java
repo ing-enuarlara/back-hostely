3accc8e35356605c7e68550277a8e5da6bc4ed0d
ing-enuarlara <enuar2110@gmail.com>
2025-06-01 21:03:10 +0200

    Se crea repository para rol y usuario

---
+++ src/main/java/com/back/hostely/repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.back.hostely.repository;
+
+import com.back.hostely.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Rol findByNombre(String nombre);
+}
