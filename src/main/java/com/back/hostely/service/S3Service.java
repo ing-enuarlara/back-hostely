99d6bd280b4072433f778f44c1ccec9496e0dc71
ing-enuarlara <enuar2110@gmail.com>
2025-06-16 02:41:47 +0200

    Se añaden servicios de S3

---
+++ src/main/java/com/back/hostely/service/S3Service.java
@@ -0,0 +1,54 @@
+package com.back.hostely.service;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    @Value("${spring.aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${spring.aws.s3.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.aws.s3.bucket-name}")
+    private String bucketName;
+
+    private S3Client s3;
+
+    @PostConstruct
+    public void init() {
+        s3 = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String uniqueFileName = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest putRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(uniqueFileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3.putObject(putRequest, RequestBody.fromBytes(file.getBytes()));
+
+        return "https://" + bucketName + ".s3.amazonaws.com/" + uniqueFileName;
+    }
+}
